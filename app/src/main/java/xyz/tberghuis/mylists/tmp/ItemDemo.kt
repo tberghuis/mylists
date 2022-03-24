e90eccdbaf91d1485cc13cf56bbc6adfb1c53817
Thomas Berghuis <thomas.berghuis@gmail.com>
2022-03-24 17:05:06 +1100

    .

---
+++ app/src/main/java/xyz/tberghuis/mylists/tmp/ItemDemo.kt
@@ -0,0 +1,32 @@
+package xyz.tberghuis.mylists.tmp
+
+import androidx.annotation.NonNull
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+
+@Entity(
+  tableName = "item_demo",
+)
+data class ItemDemo(
+  @PrimaryKey(autoGenerate = true)
+  @ColumnInfo(name = "item_id")
+  val itemId: Int = 0,
+  @NonNull @ColumnInfo(name = "item_text") val itemText: String,
+  @NonNull @ColumnInfo(name = "item_order") val itemOrder: Int
+)
+
+@Dao
+interface ItemDemoDao {
+
+  @Query("SELECT * FROM item_demo WHERE item_id = :itemId ORDER BY item_order")
+  fun getAll(itemId: Int): Flow<List<ItemDemo>>
+
+  @Insert
+  suspend fun insertAll(vararg items: ItemDemo)
+
+  @Delete
+  suspend fun delete(vararg items: ItemDemo)
+
+//  @Query("UPDATE myitem set myitem_text = :myitemText WHERE myitem_id = :myitemId")
+//  suspend fun updateMyitemText(myitemText: String, myitemId: Int)
+}
