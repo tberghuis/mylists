1a4dc889cc0a74436887f04029ed828e6ab2f5c9
Thomas Berghuis <thomas.berghuis@gmail.com>
2022-03-30 08:41:56 +1100

    .

--- app/src/main/java/xyz/tberghuis/mylists/tmp/migration/migration.kt
+++ app/src/main/java/xyz/tberghuis/mylists/tmp/migration/migration.kt
@@ -1,7 +1,14 @@
 package xyz.tberghuis.mylists.tmp.migration
 
+import android.database.sqlite.SQLiteDatabase
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import xyz.tberghuis.mylists.util.logd
 
 class MigrationSingleton {
   val helloMigration = "hello migration"
@@ -11,5 +18,23 @@ val ms = MigrationSingleton()
 
 @Composable
 fun MigrationScreen() {
-  Text("hello migration")
+  val scope = rememberCoroutineScope()
+
+  Column {
+    Button(onClick = {
+      logd("run sql")
+      scope.launch(Dispatchers.IO) {
+        openDb()
+      }
+    }) {
+      Text("run sql test")
+    }
+  }
+}
+
+fun openDb() {
+  val dbpath = "/data/data/xyz.tberghuis.mylists/databases/bak.mylists.db"
+  val db = SQLiteDatabase.openDatabase(dbpath, null, SQLiteDatabase.OPEN_READWRITE)
+  val result = db.rawQuery("select * from mylist", null)
+  logd("result $result")
 }
