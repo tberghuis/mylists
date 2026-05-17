1f50af5ebcb18a3258388f29d4147f8f9d14253d
Thomas Berghuis <thomas.berghuis@gmail.com>
2026-05-17 10:06:42 +1000

    .

---
+++ app/src/main/java/xyz/tberghuis/mylists/tmp/BackupScreen.kt
@@ -0,0 +1,107 @@
+package xyz.tberghuis.mylists.tmp
+
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import androidx.activity.compose.LocalActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import org.koin.androidx.compose.koinViewModel
+import xyz.tberghuis.mylists.components.BackupButton
+import xyz.tberghuis.mylists.components.ImportButton
+import xyz.tberghuis.mylists.util.logd
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BackupScreen(
+  viewModel: BackupViewModel = koinViewModel(),
+) {
+  Scaffold(topBar = {
+    TopAppBar(
+      // TODO back/up home arrow
+      title = { Text("Backup") }
+    )
+  }) { paddingValues ->
+    Column(
+      modifier = Modifier
+        .padding(paddingValues)
+        .padding(10.dp),
+      verticalArrangement = Arrangement.spacedBy(10.dp)
+    ) {
+      Row(horizontalArrangement = Arrangement.spacedBy(10.dp)) {
+        BackupButton()
+        ImportButton()
+      }
+    }
+  }
+  ImportAlertDialog()
+}
+
+@Composable
+fun ImportAlertDialog(
+  vm: BackupViewModel = koinViewModel(),
+) {
+  val activity = LocalActivity.current
+  val close = { vm.importDialog = false }
+  val launcher =
+    rememberLauncherForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+      logd("rememberLauncherForActivityResult $result")
+      logd("launcher result ${result.resultCode}")
+      when (result.resultCode) {
+        RESULT_OK -> {
+          logd("result.data ${result.data}")
+          logd("result.data.data ${result.data?.data}")
+          result.data?.data?.let { vm.import(activity!!, it) }
+        }
+      }
+    }
+
+  fun import() {
+    val intent = Intent(Intent.ACTION_GET_CONTENT).apply {
+      type = "*/*"
+      addCategory(Intent.CATEGORY_OPENABLE)
+    }
+    launcher.launch(intent)
+  }
+
+  if (vm.importDialog) {
+    AlertDialog(
+      onDismissRequest = close,
+      title = {
+        Text(text = "Warning")
+      },
+      text = {
+        Text("Importing will delete all your current lists")
+      },
+      confirmButton = {
+        Button(
+          onClick = {
+            close()
+            import()
+          }
+        ) {
+          Text("Confirm")
+        }
+      },
+      dismissButton = {
+        Button(
+          onClick = close
+        ) {
+          Text("Cancel")
+        }
+      }
+    )
+  }
+}
---
+++ app/src/main/java/xyz/tberghuis/mylists/tmp/BackupViewModel.kt
@@ -0,0 +1,133 @@
+package xyz.tberghuis.mylists.tmp
+
+import android.app.Activity
+import android.app.Application
+import android.database.sqlite.SQLiteDatabase
+import android.net.Uri
+import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.room.Room
+import java.io.File
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+import xyz.tberghuis.mylists.DB_FILENAME
+import xyz.tberghuis.mylists.IMPORT_DB_FILENAME
+import xyz.tberghuis.mylists.data.AppDatabase
+import xyz.tberghuis.mylists.data.Mylist
+import xyz.tberghuis.mylists.service.triggerRestart
+import xyz.tberghuis.mylists.util.logd
+
+class BackupViewModel(
+  private val application: Application,
+  private val db: AppDatabase,
+) : ViewModel() {
+  var importDialog by mutableStateOf(false)
+
+  fun backup(backupFileUri: Uri) {
+    viewModelScope.launch(IO) {
+      // checkpoint
+      val query = "pragma wal_checkpoint(full)"
+      db.query(query, null).use { cursor ->
+        if (cursor.moveToFirst()) {
+          val busy = cursor.getInt(0)
+          val log = cursor.getInt(1)
+          val checkpointed = cursor.getInt(2)
+        }
+      }
+
+      val dbFile = application.getDatabasePath(DB_FILENAME)
+      application.contentResolver.openOutputStream(backupFileUri)?.use { os ->
+        dbFile.inputStream().use { fis ->
+          fis.copyTo(os)
+        }
+      }
+    }
+
+  }
+
+  fun import(activity: Activity, filePickerUri: Uri) {
+    logd("filePickerUri $filePickerUri")
+    viewModelScope.launch(IO) {
+
+      val importDbPath = application.getDatabasePath(IMPORT_DB_FILENAME).absolutePath
+      val importDbFile = File(importDbPath)
+      val inputStream = application.contentResolver.openInputStream(filePickerUri)
+
+      try {
+        // copy from filePickerUri to IMPORT_DB_FILENAME
+        inputStream?.use { input ->
+          importDbFile.outputStream().use { output ->
+            input.copyTo(output)
+          }
+        }
+
+
+//        SQLiteDatabase.openDatabase(
+//          importDbFile.path,
+//          null,
+//          SQLiteDatabase.OPEN_READONLY
+//        ).use { db ->
+//
+//          logd("db.version ${db.version}")
+//
+//
+//          val cursor = db.rawQuery("PRAGMA integrity_check", null);
+//          if (cursor.moveToFirst()) {
+//            val result = cursor.getString(0);
+//            if (result != "ok") {
+//                logd("import ok")
+//            } else {
+//                logd("import ok")
+//            }
+//          }
+//          cursor.close();
+//
+//        }
+
+
+        // test if IMPORT_DB_FILENAME is a valid room database
+        val roomImport = Room.databaseBuilder(
+          application,
+          AppDatabase::class.java,
+//          importDbFile.path
+          "tmp.db"
+        )
+          .createFromFile(importDbFile)
+
+          .build()
+        logd("before import")
+        val list = roomImport.mylistDao().getAll().first()
+        logd("after import")
+
+//        logd("before PRAGMA integrity_check")
+//        val cursor = roomImport.openHelper.writableDatabase.query("PRAGMA integrity_check")
+//        if (cursor.moveToFirst()) {
+//          val result = cursor.getString(0)
+//          logd("result $result")
+//          if (result != "ok") {
+//            // Handle corruption (e.g., delete and recreate)
+//            logd("import not ok")
+//          } else {
+//            logd("import ok")
+//          }
+//        }
+//        cursor.close()
+//
+//        logd("after PRAGMA integrity_check")
+
+//        logd("roomImport $roomImport")
+        db.close()
+        importDbFile.copyTo(application.getDatabasePath(DB_FILENAME), overwrite = true)
+        triggerRestart(activity)
+      } catch (e: Exception) {
+        Log.e("BackupViewModel", "$e")
+      }
+    }
+  }
+}
