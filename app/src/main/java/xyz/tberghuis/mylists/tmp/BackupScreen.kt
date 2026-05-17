2839d621a26a963dc6bd6c0854a489bc806e64a5
Thomas Berghuis <thomas.berghuis@gmail.com>
2026-05-17 10:25:01 +1000

    .

--- app/src/main/java/xyz/tberghuis/mylists/tmp/BackupScreen.kt
+++ app/src/main/java/xyz/tberghuis/mylists/tmp/BackupScreen.kt
@@ -1,15 +1,9 @@
 package xyz.tberghuis.mylists.tmp
 
-import android.app.Activity.RESULT_OK
-import android.content.Intent
-import androidx.activity.compose.LocalActivity
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
@@ -19,9 +13,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import org.koin.androidx.compose.koinViewModel
-import xyz.tberghuis.mylists.components.BackupButton
-import xyz.tberghuis.mylists.components.ImportButton
-import xyz.tberghuis.mylists.util.logd
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
